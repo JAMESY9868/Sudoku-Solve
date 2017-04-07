8a9350156c136a4ccfa4c590a25f810ee223a6ea
James Y <abcde201220122012@outlook.com>
2017-04-07 08:05:09 -0500

    Updated testing procedure

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -1,8 +1,12 @@
 #include "Sudoku.h"
 
 int main() {
-    Board const board = Board();
+    Board const board = Board::debug_trivial();
     
     board.printBoard();
+    debug_vuPrint(board.getRow(3));
+    debug_vuPrint(board.getCol(4));
+    debug_vuPrint(board.getBox(7));
+    
 }
 
